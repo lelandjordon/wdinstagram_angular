5e5f792160ed107c2133048e13b8cbd24668c170
Leland Jordon <lelandjordon@mac.com>
2016-05-02 21:33:54 -0400

    got post.author to show up in page.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
 "use strict";
 
 (function(){
-  angular.module("wdinstagram", [])
+  angular
+    .module("wdinstagram", [
+      "ngResource",
+    ])
+    .controller("postController", PostController);
+
+  function PostController(){
+    var vm = this;
+
+    vm.posts = [{photo_url: "blank", author:"Steve", body:"a post"},
+                {photo_url: "blank2", author:"Grasshopper", body:"another post"}
+              ];
+  }
+
 })();
