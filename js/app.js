97794ff93ff5b680331ae757d609cdaf47be450a
Leland Jordon <lelandjordon@mac.com>
2016-05-04 20:08:30 -0400

    Clean slate.  Let's 'git' started.

--- js/app.js
+++ js/app.js
@@ -6,35 +6,6 @@
       "ui.router",
       "posts"
     ])
-    .config(["$stateProvider", RouterFunction]);
-
-
-    function RouterFunction($stateProvider){
-    $stateProvider
-    .state("postIndex", {    // This name is how we reference this state, like a view, from the rest of the app.
-      url: "/posts",      // When the URL says this...
-      templateUrl: "js/index.html",   // ... the app goes here...
-      controller: "PostIndexController",  // then uses this controller to do something.
-      controllerAs: "PostIndexViewModel"
-    })
-    .state("postNew", {
-      url: "/posts/new",
-      templateUrl: "js/new.html",
-      controller: "PostNewController",
-      controllerAs: "PostNewViewModel"
-    })
-    .state("postShow", {
-      url: "/posts/:id",
-      templateUrl: "js/show.html",
-      controller: "PostShowController",
-      controllerAs: "PostShowViewModel"
-    })
-    .state("postEdit", {
-      url: "/posts/:id/edit",
-      templateUrl: "js/edit.html",
-      controller: "PostEditController",
-      controllerAs: "PostEditViewModel"
-    });
-  }
+    
 
 })();
